ac3232336cd5ca8b0d7877afaacdf6b2e3b1a910
Steve Baker <stebaker92@users.noreply.github.com>
2020-01-23 19:41:16 +0000

    Create setup-lcd.sh

---
+++ setup-lcd.sh
@@ -0,0 +1,7 @@
+sudo rm -rf LCD-show
+git clone https://github.com/goodtft/LCD-show.git
+chmod -R 755 LCD-show
+cd LCD-show/
+
+# https://github.com/goodtft/LCD-show#2-step2-clone-my-repo-onto-your-pi
+sudo ./LCD35-show 90
