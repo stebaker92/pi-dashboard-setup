ba7dc6263147eb590d3cef2acfca214c8634c099
Steve Baker <stebaker92@users.noreply.github.com>
2020-01-23 19:47:20 +0000

    Update setup-lcd.sh

--- setup-lcd.sh
+++ setup-lcd.sh
@@ -1,7 +1,8 @@
+# https://github.com/goodtft/LCD-show#2-step2-clone-my-repo-onto-your-pi
 sudo rm -rf LCD-show
 git clone https://github.com/goodtft/LCD-show.git
 chmod -R 755 LCD-show
 cd LCD-show/
 
-# https://github.com/goodtft/LCD-show#2-step2-clone-my-repo-onto-your-pi
-sudo ./LCD35-show 90
+# rotate the screen to landscape and reboot
+sudo ./LCD35-show 180
